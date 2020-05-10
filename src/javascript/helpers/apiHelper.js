1474a0bb2d4ec2b034d1ef68c9802504a3e9a611
Roman Sahan <roman.sahan@binary-studio.com>
2020-05-10 13:20:46 +0300

    small fixes

--- src/javascript/helpers/apiHelper.js
+++ src/javascript/helpers/apiHelper.js
@@ -3,14 +3,14 @@ import { fightersDetails, fighters } from './mockData';
 const API_URL = 'https://api.github.com/repos/binary-studio-academy/stage-2-es6-for-everyone/contents/resources/api/';
 const useMockAPI = true;
 
-async function callApi(endpoind, method) {
-  const url = API_URL + endpoind;
+async function callApi(endpoint, method) {
+  const url = API_URL + endpoint;
   const options = {
     method,
   };
 
   return useMockAPI
-    ? fakeCallApi(endpoind)
+    ? fakeCallApi(endpoint)
     : fetch(url, options)
         .then((response) => (response.ok ? response.json() : Promise.reject(Error('Failed to load'))))
         .then((result) => JSON.parse(atob(result.content)))
@@ -19,18 +19,18 @@ async function callApi(endpoind, method) {
         });
 }
 
-async function fakeCallApi(endpoind) {
-  const response = endpoind === 'fighters.json' ? fighters : getFighterById(endpoind);
+async function fakeCallApi(endpoint) {
+  const response = endpoint === 'fighters.json' ? fighters : getFighterById(endpoint);
 
   return new Promise((resolve, reject) => {
     setTimeout(() => (response ? resolve(response) : reject(Error('Failed to load'))), 500);
   });
 }
 
-function getFighterById(endpoind) {
-  const start = endpoind.lastIndexOf('/');
-  const end = endpoind.lastIndexOf('.json');
-  const id = endpoind.substring(start + 1, end);
+function getFighterById(endpoint) {
+  const start = endpoint.lastIndexOf('/');
+  const end = endpoint.lastIndexOf('.json');
+  const id = endpoint.substring(start + 1, end);
 
   return fightersDetails.find((it) => it._id === id);
 }
