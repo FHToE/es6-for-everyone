754e6175ff4fef273e5e9bd1f6e9821667f0967a
FHToE <64385988+FHToE@users.noreply.github.com>
2020-05-12 18:56:04 +0300

    start working variant

--- babel.config.js
+++ babel.config.js
@@ -3,14 +3,14 @@ const presets = [
     "@babel/preset-env",
     {
       targets: {
-        firefox: "74",
-        chrome: "80"
+        firefox: "60",
+        chrome: "67"
       }
     }
   ]
 ];
 
-const plugins = ["@babel/plugin-proposal-class-properties", "@babel/plugin-proposal-optional-chaining"];
+const plugins = ["@babel/plugin-proposal-class-properties"];
   
 module.exports = { 
   presets, 
